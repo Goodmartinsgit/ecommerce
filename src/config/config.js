c75b4a0cc6f936f261db5aa0086c33ffade5c859
Martins Goodluck <goodmartins43@gmail.com>
2025-12-06 14:02:31 +0100

    finaal update

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
 // API Configuration
-export const baseUrl = "https://ecombackend-xpdc.onrender.com/";
+export const baseUrl = import.meta.env.VITE_API_BASE_URL || "https://ecombackend-xpdc.onrender.com/api/";
